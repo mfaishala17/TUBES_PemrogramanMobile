ee7805a5d9c4046f8105677e1fdc42718d622c4d
Faishal <m.faishala17@gmail.com>
2024-06-23 12:13:43 +0700

    Initial Commit

---
+++ app/src/main/java/com/mfaishala/tubespemmobile/SavedScreen2.kt
@@ -0,0 +1,20 @@
+package com.mfaishala.tubespemmobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SavedScreen2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.saved_screen2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
